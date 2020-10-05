5f19583cbd0b84790697f368c0d86641148f6cd3
mphjoosten <m.p.h.joosten@student.tue.nl>
2020-10-05 19:50:27 +0200

    wrote create_group_sets, load_ground_truth and check_annotations

--- code/annotation_processing.py
+++ code/annotation_processing.py
@@ -6,6 +6,21 @@ import re
 # Definitions
 AnnotationPath = 'C:\\Users\\max\\stack\\TUE\\Sync_laptop\\OGO beeldverwerking dataset\\crowdskin\\data-clean'
 DataTypeFilename = 'data_types.csv'
+TrainPath = 'C:\\Users\\max\\stack\\TUE\\Sync_laptop\\OGO beeldverwerking dataset\\ISIC-2017_Training_Part3_GroundTruth.csv'
+ValidationPath = 'C:\\Users\\max\\stack\\TUE\\Sync_laptop\\OGO beeldverwerking dataset\\ISIC-2017_Validation_Part3_GroundTruth.csv'
+TestPath = 'C:\\Users\\max\\stack\\TUE\\Sync_laptop\\OGO beeldverwerking dataset\\ISIC-2017_Test_v2_Part3_GroundTruth.csv'
+
+# Variables
+NumGroups = 10  # Amount of groups
+NumImages = 100  # Amount of images per group
+AnnotationBalance = 0.3  # Distribution Malignant Benign for images
+# Image has to be annotated for these annotation types to be considered annotated. It is possible to choose from:
+# ['Asymmetry', 'Border', 'Color', 'Color_Categorised', 'Dermo',
+#        'Blood', 'Blue', 'Color_Fade', 'Compactness', 'Erythema', 'Red',
+#        'Black', 'Skin_Color', 'Irregular_pigmentation', 'Flaking',
+#        'Rough_Surface', 'Irregularity', 'White', 'Diameter', 'Yellow',
+#        'Brown']
+RequiredAnnotations = ['Asymmetry', 'Border', 'Color']
 
 
 def select_data(DataPath, DataTypePath, year, group):
@@ -58,5 +73,61 @@ def get_annotations(annotations_df, ISIC_ID):
     return annotation
 
 
+def load_ground_truth(Train_Path, Validation_Path, Test_Path):
+    # loads ground truth as dataframe from ISIC train, validation and test csv's
+    GT_raw_validation = pd.read_csv(Validation_Path)
+    GT_raw_test = pd.read_csv(Test_Path)
+    GT_raw_train = pd.read_csv(Train_Path)
+    GT_df = GT_raw_train.append(GT_raw_validation.append(GT_raw_test))
+    GT_df = GT_df.astype({'melanoma': 'bool', 'seborrheic_keratosis': 'bool'})
+    GT_df['malignant'] = GT_df['melanoma'] | GT_df['seborrheic_keratosis']
+    return GT_df
+
+
+def check_annotations(df_annotations, req_annotations):
+    # Checks if images are already annotated based on list req_annotations. (has to be annotated for these datatypes
+    # to be counted as an annotation). Then using load_ground_truth creates a dataframes for benign and malignant that
+    # are annotated or not annotated
+    contains_annotation = df_annotations.groupby(['ID']).apply(lambda x: sum(x['data_type'] == req_annotations[0]) > 0)
+    for i in range(len(req_annotations) - 1):
+        i += 1
+        contains_group = df_annotations.groupby(['ID']).apply(lambda x: sum(x['data_type'] == req_annotations[i]) > 0)
+        contains_annotation = contains_annotation & contains_group
+    contains_annotation = contains_annotation[contains_annotation == True].index
+
+    GT = load_ground_truth(TrainPath, ValidationPath, TestPath)
+    GT_Malignant = GT[GT['malignant'] == True].drop('malignant', axis=1)
+    GT_Benign = GT[GT['malignant'] == False].drop('malignant', axis=1)
+
+    ID_malignant = GT_Malignant[GT_Malignant['image_id'].isin(contains_annotation) == False].sort_values('image_id', ignore_index=True)
+    ID_benign = GT_Benign[GT_Benign['image_id'].isin(contains_annotation) == False].sort_values('image_id', ignore_index=True)
+    ID_malignant_annotated = GT_Malignant[GT_Malignant['image_id'].isin(contains_annotation)].sort_values('image_id', ignore_index=True)
+    ID_benign_annotated = GT_Benign[GT_Benign['image_id'].isin(contains_annotation)].sort_values('image_id', ignore_index=True)
+    return ID_malignant, ID_benign, ID_malignant_annotated, ID_benign_annotated
+
+
+def create_group_sets(df_annotations, num_groups, amount_img, class_balance, required_annotations):
+    # Devides annotations into dataframes based on the number of groups, the amount of images per group, and the
+    # distribution between malignant and benign. Returns a dictionairy containing a dataframe with ISIC images for each
+    # group number.
+    ID_malignant, ID_benign, ID_malignant_ann, ID_benign_ann = check_annotations(df_annotations, required_annotations)
+    ID_malignant = ID_malignant.append(ID_malignant_ann)
+    ID_benign = ID_benign.append(ID_benign_ann)
+    num_malignant = np.rint(amount_img * class_balance).astype(int)
+    num_benign = np.rint(amount_img * (1 - class_balance)).astype(int)
+    ann_dict = {}
+    for i in range(num_groups):
+        i += 1
+        df_group_benign = ID_benign.iloc[i * num_benign:i * num_benign + 1]
+        df_group_malignant = ID_malignant.iloc[i * num_benign:i * num_malignant + 1]
+        df_group = df_group_benign.append(df_group_malignant)
+        if i < 10:
+            ann_dict['group0' + str(i)] = df_group
+        else:
+            ann_dict['group' + str(i)] = df_group
+    return ann_dict
+
+
 df = create_annotation_df(AnnotationPath, DataTypeFilename)
 test_annotation = get_annotations(df, 'ISIC_0014092')
+images_per_group = create_group_sets(df, NumGroups, NumImages, AnnotationBalance, RequiredAnnotations)
