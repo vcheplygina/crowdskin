22e34d3851373b88bf394f03cd17c3a0209d3587
mphjoosten <m.p.h.joosten@student.tue.nl>
2020-11-09 16:25:54 +0100

    Clean code, Change paths, Add comments, Add ISIC GT files to repository

--- code/annotation_processing_functions.py
+++ code/annotation_processing_functions.py
@@ -4,18 +4,22 @@ import os
 import re
 
 
-def select_data(DataPath, DataTypePath, year, group):
+def select_data(data_path, data_type_path, year, group):
     """For a given file convert every value to new dataframe with columns:
     ['ID', 'group_number', 'year', 'annotator', 'orig_column', 'data_type', 'data']"""
 
+    # Read cleaned student files and datatype files
     annotations_df = pd.DataFrame()
-    group_df = pd.read_csv(DataPath, delimiter=';').dropna(axis=0, how='all').dropna(axis=1, how='all')
-    type_df = pd.read_csv(DataTypePath, delimiter=';').dropna(axis=0, how='all').dropna(axis=1, how='all')
+    group_df = pd.read_csv(data_path, delimiter=';').dropna(axis=0, how='all').dropna(axis=1, how='all')
+    type_df = pd.read_csv(data_type_path, delimiter=';').dropna(axis=0, how='all').dropna(axis=1, how='all')
 
+    # Initiate arrays
     df_length = group_df.shape[0]
     IDs = group_df.iloc[:, 0]
     group_array = np.full(df_length, group)
     year_array = np.full(df_length, year)
+
+    # Select annotations from file and generate a dataframe from the filled arrays
     for i in range(1, group_df.shape[1]):
         column_data = group_df.iloc[:, i]
         data_type = type_df.loc[type_df['group_num'] == group].iloc[:, i]
@@ -35,9 +39,11 @@ def create_annotation_df(annotation_path, data_type_filename):
 
     clean_annotations_df = pd.DataFrame()
 
+    # Walk trough folders containing cleaned annotations
     for subdir, dirs, files in os.walk(annotation_path):
         files_data = [s for s in files if "group" in s]
         data_types_path = os.path.join(subdir, data_type_filename)
+        # Select cleaned data using select_data for each annotation file
         for file in files_data:
             year = subdir[-9:]
             file_path = os.path.join(subdir, file)
@@ -55,14 +61,17 @@ def get_annotations(annotations_df, ISIC_ID):
     return annotation
 
 
-def load_ground_truth(Train_Path, Validation_Path, Test_Path, malignant_lesion):
+def load_ground_truth(train_path, validation_path, test_path, malignant_lesion):
     """loads ground truth as dataframe from ISIC train, validation and test csv's"""
 
-    GT_raw_validation = pd.read_csv(Validation_Path)
-    GT_raw_test = pd.read_csv(Test_Path)
-    GT_raw_train = pd.read_csv(Train_Path)
+    # Read ISIC ground truth data drom csv
+    GT_raw_validation = pd.read_csv(validation_path)
+    GT_raw_test = pd.read_csv(test_path)
+    GT_raw_train = pd.read_csv(train_path)
     GT_df = GT_raw_train.append(GT_raw_validation.append(GT_raw_test))
     GT_df = GT_df.astype({'melanoma': 'bool', 'seborrheic_keratosis': 'bool'})
+
+    # Generate a dataframe with ISIC ID's and if the lesions is considerd malignant (can be chosen by user)
     if malignant_lesion == 'both':
         GT_df['malignant'] = GT_df['melanoma'] | GT_df['seborrheic_keratosis']
     elif malignant_lesion == 'keratosis':
@@ -77,11 +86,15 @@ def load_ground_truth(Train_Path, Validation_Path, Test_Path, malignant_lesion):
 def annotation_stats(df_annotations):
     """Prints a few statistics for the dataframe containing all annotations"""
 
+    # Group dataframe by data_type and ID
     df_annotations_grouped = df_annotations.groupby('data_type')
     df_annotations_grouped_id = df_annotations.groupby(['data_type', 'ID'])
+
+    # Calculate ammount, mean and minimum of annotations per category
     stats_df = pd.DataFrame(df_annotations_grouped['data'].count()).rename({'data': 'ann_count'}, axis=1)
     stats_df['mean_ann_img'] = df_annotations_grouped_id['data'].count().mean(level=0)
     stats_df['min_ann_img'] = df_annotations_grouped_id['data'].count().min(level=0)
+
     print('Annotation statistics: \n')
     print(stats_df)
 
@@ -90,28 +103,33 @@ def drop_annotation_count_categories(df, categories, count):
     """Checks which categories and amounts of annotations should be available before the image is considered annotated.
     Returns an array of ID's that are considered annotated"""
 
+    # Group dataframe by data_type
     grouped_df = df.groupby(['ID', 'data_type'])
     grouped_count = grouped_df['data_type'].count()
     cat_index = grouped_count.index.get_level_values(level=1)
 
-    grouped_df_cat = grouped_count[cat_index == categories[0]]
-    ID = grouped_df_cat.index.get_level_values(level=0)[grouped_df_cat >= count[0]]
-    for i in range(1, len(categories)):
+    # Keep ISIC id's that contain the categories and amounts of images requested by the user
+    ID = grouped_count.index.get_level_values(level=0)
+    for i in range(0, len(categories)):
         grouped_df_cat = grouped_count[cat_index == categories[i]]
         ID_min_count = grouped_df_cat.index.get_level_values(level=0)[grouped_df_cat >= count[i]]
-        ID = ID[ID.isin(ID_min_count)]
+        ID = ID[ID.isin(ID_min_count)].unique()
     return ID
 
 
-def categorise_annotations(image_ids, TrainPath, ValidationPath, TestPath, malignant_lesion):
+def categorise_annotations(image_ids, train_path, validation_path, test_path, malignant_lesion):
     """ Checks if images are already annotated based on list req_annotations. (has to be annotated for these datatypes
     to be counted as an annotation). Then using load_ground_truth creates dataframes for benign and malignant that
     are annotated or not annotated """
 
-    GT = load_ground_truth(TrainPath, ValidationPath, TestPath, malignant_lesion)
+    # Read ground truth
+    GT = load_ground_truth(train_path, validation_path, test_path, malignant_lesion)
+
+    # Divide ground truth in malignant and benign
     GT_Malignant = GT[GT['malignant'] == True].drop('malignant', axis=1)
     GT_Benign = GT[GT['malignant'] == False].drop('malignant', axis=1)
 
+    # Divide annotated and not annotated data into malignant and benign dataframe
     ID_malignant = GT_Malignant[GT_Malignant['image_id'].isin(image_ids) == False].sort_values('image_id',
                                                                                                ignore_index=True)
     ID_benign = GT_Benign[GT_Benign['image_id'].isin(image_ids) == False].sort_values('image_id',
@@ -128,10 +146,16 @@ def create_group_sets(ID_malignant, ID_benign, ID_malignant_ann, ID_benign_ann, 
     distribution between malignant and benign. Returns a dictionairy containing a dataframe with ISIC images for each
     group number."""
 
+    # Generate malignant and benign dataframes where unannotated images are the first rows
     ID_malignant = ID_malignant.append(ID_malignant_ann)
     ID_benign = ID_benign.append(ID_benign_ann)
+
+    # Calculate the number of benign and malignant images using the ratio provided by the user
     num_malignant = np.rint(amount_img * class_balance).astype(int)
     num_benign = np.rint(amount_img * (1 - class_balance)).astype(int)
+
+    # Generate a dict where every group is assigned a dataframe containing ISIC id's. These dataframes contain the
+    # amount of images per group according to the requested malignant/benign ratio
     ann_dict = {}
     for i in range(num_groups):
         i += 1
@@ -145,14 +169,17 @@ def create_group_sets(ID_malignant, ID_benign, ID_malignant_ann, ID_benign_ann, 
     return ann_dict
 
 
-def save_group_sets(ID_group, Save_folder):
+def save_group_sets(ID_group, save_folder):
     """Saves images per group as csv with ground truth and without in assigned folder"""
 
-    if not os.path.exists(Save_folder):
-        os.makedirs(Save_folder)
+    # Make folder if it does not exist yet
+    if not os.path.exists(save_folder):
+        os.makedirs(save_folder)
+
+    # Save images per group
     for group in ID_group:
-        GT_save_name = os.path.join(Save_folder, group + '.csv')
+        GT_save_name = os.path.join(save_folder, group + '.csv')
         GT_name = group + '_GroundTruth.csv'
-        save_name = os.path.join(Save_folder, GT_name)
+        save_name = os.path.join(save_folder, GT_name)
         ID_group[group].to_csv(save_name, index=False)
         ID_group[group].drop(['melanoma', 'seborrheic_keratosis'], axis=1).to_csv(GT_save_name, index=False)
